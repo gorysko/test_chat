5ea80dd8091ad44980898ef53a42fb6103e4a27b
gorysko <gorysko@gmail.com>
2015-02-05 22:53:35 +0200

    test app

---
+++ chat.py
@@ -0,0 +1,12 @@
+from gevent import monkey
+from flask import Flask
+
+monkey.patch_all()
+
+application = Flask(__name__)
+application.debug = True
+application.config['PORT'] = 5000
+
+@application.route('/', methods=['GET',])
+def landing():
+    return "TEST"
